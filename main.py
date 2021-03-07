62f6293d4817a66d59e66eded4a0e77df3028986
maxdeleon <maxdeleon@vt.edu>
2021-03-07 16:05:11 -0500

    Changed from stats module to sklearn. Fixed coefficient issue.

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import pandas as pd
-import statsmodels.api as sm
 import matplotlib.pyplot as plt
-import numpy as np
+from sklearn import linear_model
+
 
 balance_sheet = pd.read_csv('natGasData.csv')
 balance_sheet.index = balance_sheet['Month']
 balance_sheet.drop(['Month'], axis=1)
-balance_sheet =balance_sheet.dropna()
+balance_sheet = balance_sheet.dropna()
 
-print(balance_sheet.columns)
+#print(balance_sheet.columns)
 
 residential_regression_y = balance_sheet['Residential Consumption']
 electrical_regression_y = balance_sheet['Electric Power Consumption']
 
 months = ['Feb','Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 trend = 'trend'
+X = balance_sheet[months]
+X = sm.add_constant(X)
+Y = residential_regression_y
 
 
-model = sm.GLS(residential_regression_y, balance_sheet[months]).fit()
-predictions = model.predict(balance_sheet[months])
-model_summary = model.summary()
-print(model_summary)
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+print('Intercept: \n', regr.intercept_)
+print('Coefficients: \n', regr.coef_)
+
+predictions = regr.predict(X)
+
 
 balance_sheet['res model'] = predictions
+
+
+
+
 
 
 # Doesnt match up properly with teh excel sheet
