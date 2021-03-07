3e7e71b3c6910b53e947d1d9f38e1bb0ae6b6638
maxdeleon <maxdeleon@vt.edu>
2021-03-07 15:42:55 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import numpy as np
+
+balance_sheet = pd.read_csv('natGasData.csv')
+balance_sheet.index = balance_sheet['Month']
+balance_sheet.drop(['Month'], axis=1)
+balance_sheet =balance_sheet.dropna()
+
+print(balance_sheet.columns)
+
+residential_regression_y = balance_sheet['Residential Consumption']
+electrical_regression_y = balance_sheet['Electric Power Consumption']
+
+months = ['Feb','Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+trend = 'trend'
+
+
+model = sm.GLS(residential_regression_y, balance_sheet[months]).fit()
+predictions = model.predict(balance_sheet[months])
+model_summary = model.summary()
+print(model_summary)
+
+balance_sheet['res model'] = predictions
+
+
+
+ax = plt.gca()
+balance_sheet.plot(y='Residential Consumption', color='blue', ax=ax, figsize=(10,5))
+balance_sheet.plot(y='res model', color='red',style='--', ax=ax)
+
+
+plt.show(figsize=(20,7))
+
+
